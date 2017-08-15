f990ff09e4230a388f65efa44dfb2603ea9f85d4
Kylepayen <30319619+Kylepayen@users.noreply.github.com>
2017-08-15 14:08:22 -0400

    Commit #31

--- microblog-version-0.4/app/views.py
+++ microblog-version-0.4/app/views.py
@@ -5,7 +5,7 @@ from app import app
 from .forms import LoginForm
 from werkzeug.utils import secure_filename
 import DataTracking as DT
-import SeperationAl as SepAl
+import SeperationAL as SepAl
 
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
