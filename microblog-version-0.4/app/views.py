6318bde0884a2cf3104d63575efdb75873b9aa2b
Kylepayen <30319619+Kylepayen@users.noreply.github.com>
2017-08-16 13:06:15 -0400

    git commit #48

--- microblog-version-0.4/app/views.py
+++ microblog-version-0.4/app/views.py
@@ -33,7 +33,7 @@ def upload_file():
     if request.method == 'POST':
         username=str(request.form['username'])
         path=str(request.form['fpath']+'/'+request.form['fname'])
-        print(username, path)
+        print('dsf', 'fds')
         DT.InputData(username,path,0)
         # check if the post request has the file part
         print (request.files)
