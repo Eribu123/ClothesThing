19e3fa17f9a04572fec4fd9f7e18da42720c4036
Kylepayen <30319619+Kylepayen@users.noreply.github.com>
2017-08-16 12:58:15 -0400

    git commit #46

--- microblog-version-0.4/DataTracking.py
+++ microblog-version-0.4/DataTracking.py
@@ -8,7 +8,7 @@ from PIL import Image
 import warnings
 # warnings.filterwarnings("ignore", category=DeprecationWarning) 
 # print 'asodfausdfaiosudhfuoasdhfuisdahuilasdfhui'
-conn=sqlite3.connect('DataBase1.db')
+conn=sqlite3.connect('DataBase123.db')
 sqlite3.connect(":memory:", check_same_thread=False)
 
 c=conn.cursor()
@@ -34,7 +34,7 @@ Spath5 = '/Users/Aubrey/Desktop/clothes/Shirts/ResizedShirts/TestShirt5.jpg'
 Ppath5='/Users/Aubrey/Desktop/clothes/pants/ResizedPants/TestPants5.jpg'
 
 score=0
-c.execute("CREATE TABLE A (I Integer)")
+c.execute("CREATE TABLE Abc (I Integer)")
 def AddToUserInputData(ThisUser,Uinput,Utype): #These are what were going to be getting from front end everytime the usr uses the product
 	# score=0 
 	# print _input
